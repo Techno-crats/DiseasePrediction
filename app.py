284dde1c8de4bf90f026f165092450ac5ba3bf62
jackie <99sharmatushar@gmail.com>
2020-12-23 14:34:43 +0530

    Add basic functioning without styles

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
+import flask
 from flask import Flask, request, jsonify, render_template
+import joblib
 import pickle
 
 app = Flask(__name__)
@@ -9,19 +11,26 @@ model = pickle.load(open('disease_prediction.pkl','rb'))
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return flask.render_template('index.html')
+
+def diseasePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1,132)
+    model = joblib.load('disease_prediction.pkl')
+    result = model.predict(to_predict)
+    # loaded_model = pickle.load(open("disease_prediction.pkl","rb"))
+    # result = loaded_model.predict(to_predict)
+    return result[0]
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    
-    features = [x for x in request.form.values()]     
-    features = [np.array(features)]  
+    if request.method == 'POST':
+        my_list=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        result=list(map(int, request.form.to_dict().keys()))
+        for i in result:
+            my_list[i-1]=1;
+        result = diseasePredictor(my_list)
+        return render_template("result.html",prediction=result)
 
-    column_names=[]
-    final_features=pd.DataFrame(features,columns=column_names)
-    prediction= model.predict(final_features)
-
-    return jsonify(prediction[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
